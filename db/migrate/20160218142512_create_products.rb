b4f0bc441f00cec9c32f09c0d903f60689abdc34
Tori <victoria.ashley.wood@gmail.com>
2016-02-18 09:43:29 -0500

    git init

---
+++ db/migrate/20160218142512_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :quantity
+      t.integer :rating
+      t.integer :category_id
+
+      t.timestamps null: false
+    end
+  end
+end
