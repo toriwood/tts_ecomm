2d7d31f7c62a72344e5851169030f39114d0cdbd
Tori <victoria.ashley.wood@gmail.com>
2016-02-19 13:41:35 -0500

    fixed error with navbar categories on devise views by adding a helper method into the application controller for the categories variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+	helper_method :categories
+
+	def categories
+		@categories = Category.order('name ASC').all
+	end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
