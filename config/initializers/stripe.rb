c390ea3586a3d7afdcbb6f907f0e429ba09dcaf2
Tori <victoria.ashley.wood@gmail.com>
2016-02-24 11:25:41 -0500

    updated stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ['stripe_publishable_key'],
-  :secret_key      => ['stripe_api_key']
+  :publishable_key => ENV['stripe_publishable_key'],
+  :secret_key      => ENV['stripe_api_key']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
